1f691f23660dd4a3376929f8534ef00e4c4837e7
Jiheon Lim <53568983+JiHeonLim@users.noreply.github.com>
2024-12-07 21:43:05 +0900

    Initial commit

---
+++ client/src/Root.jsx
@@ -0,0 +1,250 @@
+import React from 'react';
+import "./global.scss";
+import "./index.scss";
+const Root = () => {
+React.useEffect(() => {
+// Initialize the code
+return () => {}
+}, [])
+return (
+<div className={'body-186691'}>
+    <div className={'main'}>
+        <div className={'section'}>
+            <div className={'div'}>
+                <div className={'img'}>
+                </div>
+            </div>
+            <div className={'div-1'}>
+                <div className={'div-2'}>
+                    <div className={'div-3'}>
+                        <div className={'h1'}>
+                            <div className={'future-is-now'}>
+                                Future Connected
+                            </div>
+                        </div>
+                        <div className={'p'}>
+                            <div className={'experience-the-next-generation-of-digital-innovation-where-technology-meets-imagination-in-the-most-spectacular-way'}>
+                                <div><span>새로운 여정이 시작됐습니다.<div className="_p"></div>어서 탑승하세요.</span></div>
+                            </div>
+                        </div>
+                        <div className={'div-4'}>
+                            <div className={'button'}>
+                                <div className={'get-started'}>
+                                    Get Started
+                                </div>
+                            </div>
+                            <div className={'button-1'}>
+                                <div className={'learn-more'}>
+                                    Learn More
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div className={'section-1'}>
+            <div className={'div-5'}>
+                <div className={'div-6'}>
+                    <div className={'div-7'}>
+                        <div className={'h2'}>
+                            <div className={'the-future-of-technology'}>
+                                The Collapsing World
+                            </div>
+                        </div>
+                        <div className={'p-1'}>
+                            <div className={'were-building-the-next-generation-of-digital-experiences-our-products-combine-cutting-edge-technology-with-intuitive-design'}>
+                                <div><span>기존의 질서는 무너지고,<div className="_p"></div>새로운 시대가 도래했습니다.<div className="_p"></div>
+                                        <div className="_p"></div>기술과 정보의 확장으로<div className="_p"></div>우리는 예측할 수 없는 미래를 맞이하고 있습니다.<div className="_p"></div>
+                                        <div className="_p"></div>변화의 물결은 모든 영역에서 일어나고 있으며,<div className="_p"></div>그 속도는 점점 더 빨라지고 있습니다.
+                                    </span></div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className={'magicon'}>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div className={'section-2'}>
+            <div className={'div-8'}>
+                <div className={'div-9'}>
+                    <div className={'div-10'}>
+                        <div className={'div-11'}>
+                            <div className={'h2-1'}>
+                                <div className={'the-future-of-technology-1'}>
+                                    Polymaths Awaken
+                                </div>
+                            </div>
+                            <div className={'p-2'}>
+                                <div className={'were-building-the-next-generation-of-digital-experiences-our-products-combine-cutting-edge-technology-with-intuitive-design-1'}>
+                                    <div><span>더 이상 한 가지에만 집중하는 시대가 아닙니다. <div className="_p"></div>한정된 지식으로는 더 이상 경쟁할 수 없습니다.<div className="_p"></div>
+                                            <div className="_p"></div>다능인이 태어나는 세상이 도래했습니다.
+                                        </span></div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <img src="https://image-resource.creatie.ai/145871968104183/145871968104185/35d87480d5daa2ef6457d005c9072aad.png" className={'magicon-1'} />
+                </div>
+            </div>
+        </div>
+    </div>
+    <div className={'footer'}>
+        <div className={'div-12'}>
+            <div className={'div-13'}>
+                <div className={'div-14'}>
+                    <div className={'p-3'}>
+                        <div className={'building-the-future-of-digital-experiences'}>
+                            Building the future of digital experiences.
+                        </div>
+                    </div>
+                    <div className={'img-1'}>
+                    </div>
+                </div>
+                <div className={'div-15'}>
+                    <div className={'h3'}>
+                        <div className={'products'}>
+                            Products
+                        </div>
+                    </div>
+                    <div className={'ul'}>
+                        <div className={'li'}>
+                            <div className={'a'}>
+                                <div className={'neural-interface'}>
+                                    Test1
+                                </div>
+                            </div>
+                        </div>
+                        <div className={'li-1'}>
+                            <div className={'a-1'}>
+                                <div className={'quantum-ai'}>
+                                    Test2
+                                </div>
+                            </div>
+                        </div>
+                        <div className={'li-2'}>
+                            <div className={'a-2'}>
+                                <div className={'virtual-reality'}>
+                                    Test3
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div className={'div-16'}>
+                    <div className={'h3-1'}>
+                    </div>
+                    <div className={'ul-1'}>
+                        <div className={'li-3'}>
+                            <div className={'a-3'}>
+                                <div className={'about-us'}>
+                                    About us
+                                </div>
+                            </div>
+                        </div>
+                        <div className={'li-4'}>
+                            <div className={'a-4'}>
+                                <div className={'careers'}>
+                                    Careers
+                                </div>
+                            </div>
+                        </div>
+                        <div className={'li-5'}>
+                            <div className={'a-5'}>
+                                <div className={'contact'}>
+                                    Contact
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div className={'div-17'}>
+                    <div className={'h3-2'}>
+                        <div className={'connect'}>
+                            Connect
+                        </div>
+                    </div>
+                    <svg id="1:031" className={'div-18'}></svg>
+                </div>
+            </div>
+            <div className={'div-19'}>
+                <div className={'p-4'}>
+                    <div className={'text--2024-cyber-future-all-rights-reserved'}>
+                        © 2024 OMNI CHAIN. All rights reserved.
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div className={'nav'}>
+        <div className={'div-20'}>
+            <div className={'div-21'}>
+                <div className={'frame-11'}>
+                    <div className={'div-22'}>
+                        <div className={'img-2'}>
+                        </div>
+                    </div>
+                    <div className={'div-23'}>
+                        <div className={'frame-9'}>
+                            <div className={'home'}>
+                                Home
+                            </div>
+                            <div className={'about'}>
+                                About
+                            </div>
+                            <div className={'journey'}>
+                                Journey
+                            </div>
+                            <div className={'community'}>
+                                Community
+                            </div>
+                            <div className={'contact-1'}>
+                                Contact
+                            </div>
+                        </div>
+                    </div>
+                    <div className={'div-24'}>
+                        <div className={'button-2'}>
+                            <div className={'connect-wallet'}>
+                                Sign Up
+                            </div>
+                        </div>
+                        <div className={'button-3'}>
+                            <div className={'connect-wallet-1'}>
+                                Login
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div className={'section-3'}>
+        <div className={'div-25'}>
+            <div className={'div-26'}>
+                <div className={'div-27'}>
+                    <div className={'h2-2'}>
+                        <div className={'the-future-of-technology-2'}>
+                            Future Connected
+                        </div>
+                    </div>
+                    <div className={'p-5'}>
+                        <div className={'were-building-the-next-generation-of-digital-experiences-our-products-combine-cutting-edge-technology-with-intuitive-design-2'}>
+                            <div><span>미래는 다능인에게 열려 있습니다.<div className="_p"></div>다양한 기술을 습득하고,<div className="_p"></div>여러 분야를 아우를 수 있는 능력을 키워야 합니다.<div className="_p"></div>
+                                    <div className="_p"></div>다능인이 되지 않으면, 변화의 물결 속에서 도태될 것입니다. <div className="_p"></div>미래를 선도하려면 지금 당장 준비해야 합니다.
+                                </span></div>
+                        </div>
+                    </div>
+                </div>
+                <div className={'div-28'}>
+                    <div className={'img-3'}>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+);
+};
+export default Root
